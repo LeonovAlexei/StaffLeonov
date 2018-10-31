4aea6081da78f0885aa35c5aee2eef7a7763c9b2
AlexLeonov <baksleo@gmail.com>
2018-10-31 14:55:56 +0300

    Создал проект перечислил поля в классе Employee

---
+++ StaffLeonov/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffLeonov
+{
+    class Employee
+    {
+        public int number;
+        public string lastNeme;
+        public string firstName;
+        public string patronymic;
+        public DateTime birthday;
+        public int passportSerNum;
+        public DateTime passportDateOfIssue;
+        public string passporIssuedBy;
+        public string placeOfResidence;
+        public string policyNumber;
+        public string birthplace;
+
+
+
+
+    }
+}
