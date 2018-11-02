10ba968c057c9ecb6203de69b904cf9119a67ea3
AlexLeonov <baksleo@gmail.com>
2018-11-03 01:51:45 +0300

    Вывел на экран сущности

--- StaffLeonov/Employee.cs
+++ StaffLeonov/Employee.cs
@@ -1,27 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StaffLeonov
 {
-    class Employee
+    public class Employee
     {
-        public int number;
-        public string lastNeme;
-        public string firstName;
-        public string patronymic;
-        public DateTime birthday;
-        public int passportSerNum;
-        public DateTime passportDateOfIssue;
-        public string passporIssuedBy;
-        public string placeOfResidence;
-        public string policyNumber;
-        public string birthplace;
+        public int Number { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public string PassportSerNum { get; set; }
+        public DateTime PassportDateOfIssue { get; set; }
+        public string PassporIssuedBy { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public string PolicyNumber { get; set; }
+        public string Birthplace { get; set; }
+        public int Departamentnumber { get; set; }
+        public bool DepartamentHead { get; set; }
 
 
 
+        public Employee( int number, string lastName,string firstName,string patronymic,DateTime birthday,
+            string passportSerNum, DateTime passportDateOfIssue, string passporIssuedBy, string placeOfResidence,
+             string policyNumber, string birthplace, int departamentnumber, bool departamentHead)
+        {
+            this.Number = number;
+            this.LastName = lastName;
+            this.FirstName = firstName;
+            this.Patronymic = patronymic;
+            this.Birthday = birthday;
+            this.PassportSerNum = passportSerNum;
+            this.PassportDateOfIssue = passportDateOfIssue;
+            this.PassporIssuedBy = passporIssuedBy;
+            this.PlaceOfResidence = placeOfResidence;
+            this.PolicyNumber = policyNumber;
+            this.Birthplace = birthplace;
+            this.Departamentnumber = departamentnumber;
+            this.DepartamentHead = departamentHead;
+        }
+        public Employee() { }
+
+
+        public override string ToString()
+        {
+            return $"{Number},{LastName},{FirstName},{Patronymic},{Birthday},{PassportSerNum},{PassportDateOfIssue},{PassporIssuedBy},{PlaceOfResidence},{PolicyNumber},{Birthplace},{Departamentnumber},{DepartamentHead}";
+        }
 
     }
+
 }
--- StaffLeonov/MainWindow.xaml.cs
+++ StaffLeonov/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,55 @@ namespace StaffLeonov
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee>  colempl = new ObservableCollection<Employee>();
+        
+       
         public MainWindow()
         {
+
             InitializeComponent();
+            LoadEmployee();
+            Department.LoadDepartaments();
+
+           this.comboBox.ItemsSource = Department.departments;
+           
+        }
+        
+        public  void LoadEmployee()
+        {
+            StreamReader sr = new StreamReader("../../Employee.txt");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split('\t');
+
+                    //colempl.Add(new Employee(int.Parse(s[0]), s[1], s[2], s[3], Convert.ToDateTime(s[4]), s[5], Convert.ToDateTime(s[6]), s[7], s[8], s[9], s[10], Convert.ToInt32(s[11]), Convert.ToBoolean(s[12])));
+                    this.lv.Items.Add(new Employee { Number = int.Parse(s[0]), LastName = s[1], FirstName = s[2], Patronymic = s[3], Birthday = Convert.ToDateTime(s[4]), PassportSerNum = s[5], PassportDateOfIssue =Convert.ToDateTime(s[6]), PassporIssuedBy = s[7], PlaceOfResidence = s[8], PolicyNumber = s[9], Birthplace = s[10], Departamentnumber = Convert.ToInt32(s[11]), DepartamentHead =Convert.ToBoolean(s[12]) });
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            sr.Close();
+           
+
+        }
+        private void lv_Selected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(e.Source.ToString());
+        }
+
+        private void lv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(e.AddedItems[0].ToString());
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
