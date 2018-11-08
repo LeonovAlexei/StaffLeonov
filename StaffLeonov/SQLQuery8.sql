c2615e665fdd8a687939a0779b58bb56c26ef3f0
AlexLeonov <baksleo@gmail.com>
2018-11-08 18:45:14 +0300

    56

---
+++ StaffLeonov/SQLQuery8.sql
@@ -0,0 +1,3 @@
+﻿SELECT Employee.IdEmployee, Employee.LastName, Employee.FirstName, Employee.Patronymic, Address.Address,Address.idAddressType
+FROM Employee,Address
+WHERE Address.IdEmployee=Employee.IdEmployee --AND Address.idAddressType=1
